c2cecc44ea5179229dbe96f5f2600a15f26ba066
Bryn Harper <bharpe02@calpoly.edu>
2025-03-01 19:04:47 -0800

    Set up project

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './stylesheets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mackenzie is a beast and likes bagels</h1>
+    </div>
+  );
+}
+
+export default App;
